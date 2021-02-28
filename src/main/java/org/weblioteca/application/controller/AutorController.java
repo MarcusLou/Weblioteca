b06874a2ad44e9c0c7aa9d5d39d23119719879f2
ra93201 <ra93201@uem.br>
2021-02-28 19:47:31 -0300

    Implementação CRUD Autor

--- src/main/java/org/weblioteca/application/controller/AutorController.java
+++ src/main/java/org/weblioteca/application/controller/AutorController.java
@@ -11,54 +11,54 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.weblioteca.application.model.Cliente;
-import org.weblioteca.application.service.ClienteService;
+import org.weblioteca.application.model.Autor;
+import org.weblioteca.application.service.AutorService;
 
 @Controller
-public class ClienteController {
+public class AutorController {
 	@Autowired
-	ClienteService clienteService;
+	AutorService autorService;
 	
-	@GetMapping("/cliente")
+	@GetMapping("/autor")
 	public String viewHomePage(Model model) {
-		return clientesPaginacao(1, "nome", "asc", model);
+		return autoresPaginacao(1, "nome", "asc", model);
 	}
 	
-	@GetMapping("/novoCliente") 
-	public String novoCliente(Model model) {
-		Cliente cliente = new Cliente();
-		model.addAttribute("cliente", cliente);
-		return "salvarCliente";
+	@GetMapping("/novoAutor") 
+	public String novoAutor(Model model) {
+		Autor autor = new Autor();
+		model.addAttribute("autor", autor);
+		return "salvarAutor";
 	}
 	
-	@PostMapping("/salvarCliente")
-	public String salvarCliente(@ModelAttribute("cliente") Cliente cliente) {
-		clienteService.salvarCliente(cliente);
+	@PostMapping("/salvarAutor")
+	public String salvarAutor(@ModelAttribute("autor") Autor autor) {
+		autorService.salvarAutor(autor);
 		return "redirect:/";
 	}
 	
-	@GetMapping("/atualizarCliente/{id}")
-	public String atualizarCliente(@PathVariable ( value = "id") Long id, Model model) {
-		Cliente cliente = clienteService.getClienteById(id);
-		model.addAttribute("cliente", cliente);
-		return "atualizarCliente";
+	@GetMapping("/atualizarAutor/{id}")
+	public String atualizarAutor(@PathVariable ( value = "id") Long id, Model model) {
+		Autor autor = autorService.getAutorById(id);
+		model.addAttribute("autor", autor);
+		return "atualizarAutor";
 	}
 	
-	@GetMapping("/deletarCliente/{id}")
-	public String deletarCliente(@PathVariable (value = "id") Long id) {
-		clienteService.deletarClienteById(id);
+	@GetMapping("/deletarAutor/{id}")
+	public String deletarAutor(@PathVariable (value = "id") Long id) {
+		autorService.deletarAutorById(id);
 		return "redirect:/";
 	}
 	
 	@GetMapping("/page/{pageNo}")
-	public String clientesPaginacao(@PathVariable (value = "pageNo") int pageNo, 
+	public String autoresPaginacao(@PathVariable (value = "pageNo") int pageNo, 
 			                        @RequestParam("sortField") String sortField,
 		                        	@RequestParam("sortDir") String sortDir,
 		                         	Model model) {
 		int pageSize = 5;
 		
-		Page<Cliente> page = clienteService.findPaginated(pageNo, pageSize, sortField, sortDir);
-		List<Cliente> listaClientes = page.getContent();
+		Page<Autor> page = autorService.findPaginated(pageNo, pageSize, sortField, sortDir);
+		List<Autor> listaAutores = page.getContent();
 		
 		model.addAttribute("currentPage", pageNo);
 		model.addAttribute("totalPages", page.getTotalPages());
@@ -68,7 +68,7 @@ public class ClienteController {
 		model.addAttribute("sortDir", sortDir);
 		model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
 		
-		model.addAttribute("listaClientes", listaClientes);
+		model.addAttribute("listaAutores", listaAutores);
 		return "index";
 	}
 }
---
+++ src/main/java/org/weblioteca/application/service/AutorServiceImpl.java
@@ -0,0 +1,61 @@
+package org.weblioteca.application.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.weblioteca.application.model.Cliente;
+import org.weblioteca.application.repository.ClienteRepository;
+
+@Service
+public class AutorServiceImpl implements ClienteService {
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Override
+	public List<Cliente> getAllClientes() {
+		return clienteRepository.findAll();
+	}
+	
+	@Override
+	public List<Cliente> findByNomeContainingIgnoreCase(String nome) {
+		List<Cliente> cliente = clienteRepository.findByNomeContainingIgnoreCase(nome);
+		return cliente;
+	}
+	
+	@Override
+	public void salvarCliente(Cliente cliente) {
+		clienteRepository.save(cliente);
+	}
+
+	@Override
+	public Cliente getClienteById(Long id) {
+		Optional<Cliente> optional = clienteRepository.findById(id);
+		Cliente cliente = null;
+		if (optional.isPresent()) {
+			cliente = optional.get();
+		} else {
+			throw new RuntimeException("Cliente nao encontrado com id = " + id);
+		}
+		return cliente;
+	}
+
+	@Override
+	public void deletarClienteById(Long id) {
+		clienteRepository.deleteById(id);
+	}
+
+	@Override
+	public Page<Cliente> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection) {
+		Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending()
+				: Sort.by(sortField).descending();
+
+		Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
+		return this.clienteRepository.findAll(pageable);
+	}
+}
