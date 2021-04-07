b026371f8cf8e8840aabdbc368d2b37997347c50
Giovane <giovane.bortolato@hotmail.com>
2021-04-06 21:52:36 -0300

    Criado metodo inicial para faturar emprestimos

--- src/main/java/org/weblioteca/application/controller/EmprestimoFaturarController.java
+++ src/main/java/org/weblioteca/application/controller/EmprestimoFaturarController.java
@@ -1,5 +1,6 @@
 package org.weblioteca.application.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +11,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.weblioteca.application.model.Emprestimo;
+import org.weblioteca.application.model.Fatura;
 import org.weblioteca.application.service.EmprestimoFaturarService;
+import org.weblioteca.application.service.FaturaService;
 
 @Controller
 public class EmprestimoFaturarController {
 	
 	@Autowired
 	EmprestimoFaturarService emprestimoFaturarService;
+	
+	@Autowired
+	FaturaService faturaService;
 
 	@GetMapping("/indexFaturarEmprestimos")
 	public String viewHomePage(Model model) {
 		return emprestimoFaturarPaginacao(1, "emprestimoId", "asc", model);
+	}
+	
+	@GetMapping("/faturarEmprestimo/{id}")
+	public String faturarEmprestimo(@PathVariable (value = "id") Long id) {
+		Emprestimo emprestimo = emprestimoFaturarService.getEmprestimoById(id);
+		Date date = new Date();
+		Fatura fatura1 = new Fatura();
+		fatura1.setClienteId(emprestimo.getCliente().getClienteId());
+		fatura1.setDataFatura((java.sql.Date) date);
+		fatura1.setDiasAtraso((int) ((emprestimo.getDataDevolvido().getTime() - emprestimo.getDataDevolucao().getTime()) / (1000*60*60*24)));
+		fatura1.setValorFatura(emprestimo.getValorTotal());
+		faturaService.salvarFatura(fatura1);
+		return "redirect:/indexFaturar";
 	}
 	
 	@GetMapping("/pageEmprestimoFaturar/{pageNo}")
