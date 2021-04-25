6cdc4467dd03623be7fecb3a43a82183cc8f595a
Giovane <giovane.bortolato@hotmail.com>
2021-04-25 16:36:14 -0300

    Refatorado inativação dos livros e front-end do Crud do Livro

--- src/main/java/org/weblioteca/application/controller/LivroController.java
+++ src/main/java/org/weblioteca/application/controller/LivroController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.weblioteca.application.model.Editora;
 import org.weblioteca.application.model.Autor;
@@ -37,7 +39,8 @@ public class LivroController {
 	
 	@GetMapping("/indexLivro")
 	public String viewHomePage(Model model) {
-		return livrosPaginacao(1, "tituloLivro", "asc", model);
+		Integer ativo = 1;
+		return livrosPaginacao(1, "tituloLivro", "asc", model, ativo);
 	}
 	
 	@GetMapping("/novoLivro") 
@@ -74,7 +77,7 @@ public class LivroController {
 	public String deletarLivro(@PathVariable (value = "id") Long id) {
 		try {
 			Livro livro = livroService.getLivroById(id);
-			livro.setExcluido(true);
+			livro.setAtivo(0);
 			livroService.salvarLivro(livro);
 			return "redirect:/indexLivro";
 		}catch (Exception $e)  {			
@@ -86,13 +89,20 @@ public class LivroController {
 	public String ativarLivro(@PathVariable (value = "id") Long id) {
 		try {
 			Livro livro = livroService.getLivroById(id);
-			livro.setExcluido(false);
+			livro.setAtivo(1);
 			livroService.salvarLivro(livro);
 			return "redirect:/indexLivro";
 		}catch (Exception $e)  {			
 			return "redirect:/mensagemLivro";	
 		}
 	}
+	
+	@RequestMapping("/indexLivro/{pesquisa}")
+    public String pesquisar(Model model, @Param("ativo") Integer ativo, @Param("pesquisa") String pesquisa) {
+        List<Livro> listaLivros = livroService.pesquisar(ativo, pesquisa);
+        model.addAttribute("listaLivros", listaLivros);
+        return "indexLivro";
+    }
 	
 	@GetMapping("/mensagemLivro") 
 	public String mensagemLivro(Model model) {
@@ -103,10 +113,13 @@ public class LivroController {
 	public String livrosPaginacao(@PathVariable (value = "pageNo") int pageNoLivro, 
 			                        @RequestParam("sortField") String sortFieldLivro,
 		                        	@RequestParam("sortDir") String sortDirLivro,
-		                         	Model model) {
+		                         	Model model,
+		                         	@Param("ativo") Integer ativo) {
 		int pageSizeLivro = 5;
-		
-		Page<Livro> pageLivro = livroService.findPaginated(pageNoLivro, pageSizeLivro, sortFieldLivro, sortDirLivro);
+		if (ativo == null) {
+			ativo = 1;
+		}		
+		Page<Livro> pageLivro = livroService.findPaginated(pageNoLivro, pageSizeLivro, sortFieldLivro, sortDirLivro, ativo);
 		List<Livro> listaLivros = pageLivro.getContent();
 		
 		model.addAttribute("currentPage", pageNoLivro);
